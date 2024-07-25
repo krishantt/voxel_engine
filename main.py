5c23b829778b7ea72a9a196bf1fd30dcd31d6403
Krishant Timilsina <154585072+krishantt@users.noreply.github.com>
2024-07-26 04:27:31 +0545

    feat:added camera

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 from settings import *
 from shader_program import ShaderProgram
 from scene import Scene
+from player import Player
 
 class VoxelEngine:
     def __init__(self):
@@ -28,11 +29,13 @@ class VoxelEngine:
         self.on_init()
 
     def on_init(self):
+        self.player = Player(self)
         self.shader_program = ShaderProgram(self)
         self.scene = Scene(self)
 
 
     def update(self):
+        self.player.update()
         self.shader_program.update()
         self.scene.update()
 
