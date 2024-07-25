96ff3961d769481e1beacc15f78350d0649c718b
Krishant Timilsina <154585072+krishantt@users.noreply.github.com>
2024-07-26 03:39:34 +0545

    shader program

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from settings import *
 import moderngl as mgl
 import pygame as pg
 import sys
+
+from settings import *
+from shader_program import ShaderProgram
 
 class VoxelEngine:
     def __init__(self):
@@ -22,15 +24,20 @@ class VoxelEngine:
         self.time = 0.0
 
         self.is_running = True
+        self.on_init()
+
+    def on_init(self):
+        self.shader_program = ShaderProgram(self)
 
 
     def update(self):
+        self.shader_program.update()
         self.delta_time = self.clock.tick()
         self.time = pg.time.get_ticks() * 0.001
         pg.display.set_caption(f"Voxel Engine | FPS: {self.clock.get_fps()}")
 
     def render(self):
-        self.ctx.clear()
+        self.ctx.clear(color=BG_COLOR)
         pg.display.flip()
 
     def handle_events(self):
--- settings.py
+++ settings.py
@@ -5,3 +5,5 @@ import math
 
 # resolution
 WIN_RES = (1600,900)
+
+BG_COLOR = (0.1, 0.16, 0.25, 1.0)
