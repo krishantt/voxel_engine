54d14828cf7f1ab0138b5c2ddaf188db79b769fb
Krishant Timilsina <154585072+krishantt@users.noreply.github.com>
2024-07-26 05:46:32 +0545

    fix:typo

--- settings.py
+++ settings.py
@@ -9,17 +9,17 @@ WIN_RES = glm.vec2(1600,900)
 #chunk
 CHUNK_SIZE = 32
 H_CHUNK_SIZE = CHUNK_SIZE // 2
-CHUNK_AREA = CHUNK_SIZE ** 2
-CHUNK_VOL = CHUNK_SIZE ** 3
+CHUNK_AREA = CHUNK_SIZE * CHUNK_SIZE
+CHUNK_VOL = CHUNK_AREA * CHUNK_SIZE
 
 
 #camera
 ASPECT_RATIO = WIN_RES.x / WIN_RES.y
 FOV_DEG = 50
 V_FOV = glm.radians(FOV_DEG)
-H_FOV = 2 * math.atan(math.tan(V_FOV / 2) * ASPECT_RATIO)
+H_FOV = 2 * math.atan(math.tan(V_FOV * 0.5) * ASPECT_RATIO)
 NEAR = 0.1
-FAR = 1000.0
+FAR = 2000.0
 PITCH_MAX =glm.radians(89.0)
 
 #player
