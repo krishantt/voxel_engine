986d372a476ec3b7d7cc14e611c296de002c168e
bigya01 <078bct032.bindu@pcampus.edu.np>
2024-07-29 11:59:30 +0545

    3D noise

--- terrain_gen.py
+++ terrain_gen.py
@@ -6,14 +6,23 @@ from settings import *
 
 @njit
 def get_height(x,z):
+
+     # island mask
+    island = 1 / (pow(0.0025 * math.hypot(x - CENTER_XZ, z - CENTER_XZ), 20) + 0.0001)
+    island = min(island, 1)
+
     a1 = CENTER_Y
     a2 = a1 * 0.5
     a4 = a1 * 0.25
-    a8 = a2 * 0.125
+    a8 = a1 * 0.125
 
 
     f1 = 0.005
     f2, f4, f8 = f1 * 2, f1 * 4, f1 * 8
+
+    if noise2(0.1 *x, 0.1 * z) < 0 :
+        a1/= 1.07
+
 
     height = 0
     height += noise2(x* f1, z* f1)* a1 + a1
@@ -22,5 +31,36 @@ def get_height(x,z):
     height += noise2(x* f8, z* f8)* a8 - a8
 
     height = max(height,1)
+    height *= island
 
     return int(height)
+
+@njit
+def get_index(x, y, z):
+    return x + CHUNK_SIZE * z + CHUNK_AREA*y
+@njit
+def set_voxel_id(voxels, x, y, z, wx, wy, wz, world_height):
+    voxel_id = 0
+
+    if wy < world_height - 1:
+        voxel_id = STONE
+    
+    else:
+        rng = int(7 * random())
+        ry = wy - rng
+        if SNOW_LVL <= ry < world_height:
+            voxel_id = SNOW
+
+        elif STONE_LVL <= ry < SNOW_LVL:
+            voxel_id = STONE
+
+        elif DIRT_LVL <= ry < STONE_LVL:
+            voxel_id = DIRT
+
+        elif GRASS_LVL <= ry < DIRT_LVL:
+            voxel_id = GRASS
+
+        else:
+            voxel_id = SAND
+
+        voxels[get_index(x,y,z)] = voxel_id
--- world_objects/chunk.py
+++ world_objects/chunk.py
@@ -57,5 +57,5 @@ class Chunk:
 
                 for y in range(local_height):
                     wy = y + cy
-                    voxels[x + CHUNK_SIZE * z + CHUNK_AREA * y] = 1
+                    set_voxel_id(voxels, x, y, z, wx, wy, wz, world_height)
 
